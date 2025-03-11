f18b487079a02ec86b3ab3a77cfbb06fcfc4d012
Fouad <fouadnassif2005@gmail.com>
2025-03-11 18:04:46 +0200

    Removing node modules and folders

--- resources/views/components/imagesLayout/2images.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['image1', 'image2'])
-<div class="flex space-y-1 flex-col">
-    <div class="w-full h-64">
-        <img src="{{$image1}}" alt="Image 1" class="object-cover w-full h-full">
-    </div>
-    <div class="w-full h-64">
-        <img src="{{$image2}}" alt="Image 2" class="object-cover w-full h-full">
-    </div>
-</div>
