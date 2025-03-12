81c13a00cbe63e4e967745c34a377b90af81c6cb
Fouad <fouadnassif2005@gmail.com>
2025-03-12 11:07:00 +0200

    cors fixes1

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3001', 'http://localhost:3000', 'https://aazimtak-frontend.vercel.app'], // Your frontend URL
+    'allowed_origins' => [
+    'http://localhost:3000',  // Local dev URL
+    'https://aazimtakk.vercel.app'  // Vercel frontend URL
+],
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
